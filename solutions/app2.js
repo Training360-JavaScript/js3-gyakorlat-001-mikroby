ca43a4a75cd902fd5b3956628d97df9c8e18d343
mikroby <rtamas@hotmail.com>
2021-11-08 22:13:05 +0100

    new version

--- solutions/app2.js
+++ solutions/app2.js
@@ -3,21 +3,28 @@ const HU = {
     return new Intl
       .DateTimeFormat('hu-HU').format(toHunDate);
   },
+  
   curreny(toHuf) {
     return new Intl
       .NumberFormat('hu-HU', { style: 'currency', currency: 'HUF' }).format(toHuf);
   },
+  
   list(array) {
-    const copy=[...array];
-    copy.splice(-1, 0, 'és');
-    return copy.join(', ').replace(', és,', ' és');    
+    return array.map((item, index,array) =>
+      index === array.length-1 ? ` és ${item}`: `, ${item}`).join('').replace(', ','');
   }
+
+  // list(array) {
+  //   const copy=[...array];
+  //   copy.splice(-1, 0, 'és');
+  //   return copy.join(', ').replace(', és,', ' és');    
+  // }
 }
 
 export default HU
 
 
-// const test = ['alma', 'körte', 'banán','eper','málna'];
+// const test = ['alma', 'körte', 'banán', 'eper', 'málna'];
 // console.log(HU.date(new Date()));
 // console.log(HU.curreny(1200))
 // console.log(HU.list(test));
